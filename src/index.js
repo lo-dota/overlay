25041b84f56a37704648fca0af282d24ce240f4d
Oleg Loutchansky <luchansodev@gmail.com>
2018-10-24 23:44:02 +0300

    add dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
@@ -5,15 +6,17 @@ let mainWindow;
 const WINDOW_CONFIG = {
   frame: false,
   transparent: true,
-  width: 400,
-  height: 400,
+  width: 350,
+  height: 180,
 };
+
+const UI_URL = process.env.UI_URL || 'https://lo-dota.github.io/ui/';
 
 function createWindow() {
   // engine_no_focus_sleep 0
   // Steam/steamapps/common/dota 2 beta/game/dota/cfg/machine_convars.vcfg
   mainWindow = new BrowserWindow(WINDOW_CONFIG);
-  mainWindow.loadURL('https://lo-dota.github.io/ui/');
+  mainWindow.loadURL(UI_URL);
   mainWindow.setAlwaysOnTop(true);
 
   mainWindow.on('closed', () => {
